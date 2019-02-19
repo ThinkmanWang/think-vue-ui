4695161d98ac78d71e5bab242d4a0582225d7c56
Thinkman Wang <wangxf1985@gmail.com>
2019-02-19 14:00:29 +0800

    update

--- src/components/popup/ad-dialog.vue
+++ src/components/popup/ad-dialog.vue
@@ -4,17 +4,20 @@
                 v-model="bVisible"
                 :modal=false
                 position="center">
-            <div class="slider" >
-                <mt-swipe :auto="3500">
-                    <mt-swipe-item v-for="item in swipeData" :key="item.pic">
-                        <img :src="item.pic" @click="toAdEvent(item)">
-                    </mt-swipe-item>
-                </mt-swipe>
+
+            <div>
+                <div class="slider" >
+                    <mt-swipe :auto="3500">
+                        <mt-swipe-item v-for="item in swipeData" :key="item.pic">
+                            <img :src="item.pic" v-on:click="toAdEvent(item)">
+                        </mt-swipe-item>
+                    </mt-swipe>
+                </div>
+
+                <!--<br />-->
+                <h2>fxxkfxxkfxxkfxxkfxxkfxxk</h2>
+                <img id="btnClose" src="./img/close-circle.png" v-on:click="dismiss" />
             </div>
-
-            <!--<br />-->
-            <img id="btnClose" src="./img/close-circle.png" v-on:click="dismiss" />
-
         </mt-popup>
     </div>
 </template>
@@ -49,72 +52,59 @@
               this.bVisible = false;
           }
           , init() {
+              // init ad items from server and show if list is not null
               this.show();
           }
           , toAdEvent (item) {
-                if (typeof this._hmt !== 'undefined') {
-                    this._hmt.push(['_trackEvent', 'banner', 'click', item.key, '-'])
-                }
-                // $.report('banner', item.key, 'click', item.key)
-                let linkBase = btoa(unescape(encodeURIComponent(item.link)))
-                if (item.link.startsWith('http')) {
-                    location.href = item.link
-                } else {
-                    location.href = this.ADDRESS + '/html/openDeepLink?urlBase=' + linkBase
-                }
+              this.dismiss();
+
+              if (typeof this._hmt !== 'undefined') {
+                  this._hmt.push(['_trackEvent', 'banner', 'click', item.key, '-'])
+              }
+
+              // $.report('banner', item.key, 'click', item.key)
+              let linkBase = btoa(unescape(encodeURIComponent(item.link)))
+              if (item.link.startsWith('http')) {
+                  // window.open(item.link);
+                  location.href = item.link
+              } else {
+                  location.href = this.ADDRESS + '/html/openDeepLink?urlBase=' + linkBase
+              }
             }
         }
     }
 </script>
 
-<style scoped>
-    .AdDialog {
-        background-color:rgba(255,255,255,0);
-    }
+<style scoped lang="scss">
+    /*.AdDialog {*/
+        /*!*width: 100%;*!*/
+        /*mt-popup {*/
+            /*!*width: 100%;*!*/
+            /*!*height: 188px;*!*/
+            /*.slider {*/
+                /*height: 188px;*/
+                /*font-size: 30px;*/
+                /*text-align: center;*/
+                /*overflow: hidden;*/
+                /*img {*/
+                    /*width: 100%;*/
+                    /*height: 100%;*/
+                /*}*/
+            /*}*/
+        /*}*/
 
-    mt-popup{
-        background-color:rgba(255,255,255,0);
-    }
+    /*}*/
 
     .slider {
         height: 188px;
-        width: 100%;
-
         font-size: 30px;
         text-align: center;
         overflow: hidden;
-        /*border-radius: 8px;*/
-        background-color:rgba(255,255,255,0);
-        /*padding: 10px;*/
-        /*transform: translate(-50%, 0);*/
-
-        /*filter:alpha(Opacity=60);*/
-        /*-moz-opacity:0.6;*/
-        /*opacity: 0.6;*/
-        margin-bottom: 100px;
+        margin-bottom: 32px;
+        img {
+            width: 100%;
+            height: 100%;
+        }
     }
 
-    img {
-        width: 100%;
-        height: 100%;
-    }
-
-    /*.AdDialog {*/
-        /*filter:alpha(Opacity=60);*/
-        /*-moz-opacity:0.6;*/
-        /*opacity: 0.6;*/
-    /*}*/
 </style>
-
-<!--<style scoped lang="sass">-->
-    <!--.slider {-->
-        <!--height: 188px;-->
-        <!--font-size: 30px;-->
-        <!--text-align: center;-->
-        <!--overflow: hidden;-->
-        <!--img {-->
-            <!--width: 100%;-->
-            <!--height: 100%;-->
-        <!--}-->
-    <!--}-->
-<!--</style>-->
