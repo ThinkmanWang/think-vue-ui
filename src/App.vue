72b864f611a4c1d08e03648bab80c924e3d39a33
Thinkman Wang <wangxf1985@gmail.com>
2019-02-19 19:47:43 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,62 @@
 <template>
   <div id="app">
     <!--<h1>中国最好的UI</h1>-->
+    <mt-header id="app-header" v-bind:title="selected">
+      <router-link to="/" slot="left">
+        <mt-button icon="back"></mt-button>
+        <mt-button @click="handleClose">关闭</mt-button>
+      </router-link>
+      <mt-button icon="more" slot="right"></mt-button>
+    </mt-header>
     <AdPopup ref="adPopup"></AdPopup>
-    <AppHeader ref="appHeader"></AppHeader>
+    <!--<AppBanner ref="appHeader"></AppBanner>-->
+
+    <!--<div>-->
+      <!--<mt-cell class="page-part" title="当前选中" :value="selected" />-->
+    <!--</div>-->
+
+    <mt-tab-container class="page-tabbar-container" v-model="selected">
+      <mt-tab-container-item id="首页">
+      </mt-tab-container-item>
+      <mt-tab-container-item id="发现">
+      </mt-tab-container-item>
+      <mt-tab-container-item id="我的">
+      </mt-tab-container-item>
+    </mt-tab-container>
+
+    <div class="page-tabbar">
+      <mt-tabbar v-model="selected" fixed>
+        <mt-tab-item id="首页">
+          <img slot="icon" src="components/img/close-circle-white-64.png">
+          外卖
+        </mt-tab-item>
+        <mt-tab-item id="发现">
+          <img slot="icon" src="components/img/close-circle-white-64.png">
+          发现
+        </mt-tab-item>
+        <mt-tab-item id="我的">
+          <img slot="icon" src="components/img/close-circle-white-64.png">
+          我的
+        </mt-tab-item>
+      </mt-tabbar>
+    </div>
   </div>
 </template>
 
 <script>
 import {EventBus} from "@/event-bus";
 import AdPopup from "@/components/popup/ad-popup";
-import AppHeader from "@/components/app-header";
+import AppBanner from "@/components/app-banner";
 
 export default {
   name: 'app'
+  , data() {
+    return {
+      selected: '首页'
+    };
+  }
   , components: {
-    AppHeader,
+    AppBanner,
     AdPopup
     // HelloWorld
   }
@@ -43,12 +85,15 @@ export default {
 </script>
 
 <style>
+#app-header {
+  height: 48px;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
 }
 </style>
---
+++ src/components/descover/descover.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>发现</h1>
+</template>
+
+<script>
+    export default {
+        name: "descover"
+    }
+</script>
+
+<style scoped>
+
+</style>
