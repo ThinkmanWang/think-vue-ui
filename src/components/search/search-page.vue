26e928d044a8f4d7109ccce64857f5be3b79636a
Thinkman Wang <wangxf1985@gmail.com>
2019-02-21 11:52:05 +0800

    update

--- src/components/search/search-page.vue
+++ src/components/search/search-page.vue
@@ -1,6 +1,16 @@
 <template>
     <div id="main">
+        <mt-header id="app-header" title="搜索" fixed="true">
+            <router-link to="/" slot="left">
+                <mt-button icon="back"></mt-button>
+                <mt-button>关闭</mt-button>
+            </router-link>
+            <mt-button icon="more" slot="right"></mt-button>
+        </mt-header>
+
         <input type="button" value="BACK" v-on:click="goBack" />
+        <br />
+        <iframe src="https://www.baidu.com" width="100%" height="100%" scrolling="no"></iframe>
     </div>
 </template>
 
@@ -17,5 +27,11 @@
 </script>
 
 <style scoped>
-
+    iframe {
+        display: block; /* iframes are inline by default */
+        background: #000;
+        border: 0; /* Reset default border */
+        height: 100vh; /* Viewport-relative units */
+        width: 95vw;
+    }
 </style>
